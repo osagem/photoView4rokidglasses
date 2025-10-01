5c0c2a88743dd88ffa4cd35e4168e93a13e583f3
osagem <osagem@gmail.com>
2025-10-02 05:35:08 +0800

    v0.1.4 支持查看删除视频

--- app/src/main/java/com/osagem/photoview4rokidglasses/MyAppGlideModule.kt
+++ app/src/main/java/com/osagem/photoview4rokidglasses/MyAppGlideModule.kt
@@ -1,8 +1,13 @@
 package com.osagem.photoview4rokidglasses
 
+
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
+@GlideModule
 class MyAppGlideModule : AppGlideModule() {
-
+    // 暂时没有配置，但为了保留文件结构
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
+    }
 }
